d82ba95c024a8e7eea477bec9141bd1e2d781de5
Gavin Sinclair <gsinclair@gmail.com>
2010-12-19 01:20:30 +1100

    Changed my mind about how #rotate works
    Shape#rotate should change the receiver, not return a copy. This is
    thoroughly described in the test/shape.rb comments, along with a path to
    implementation.

--- test/shape.rb
+++ test/shape.rb
@@ -1,58 +1,76 @@
 # Here we assume the correctness of some Shape classes (Triangle etc.) and
 # see if we can rotate them (etc.) correctly.
 #
-# shape.rotate _always_ returns a copy.
+# shape.rotate alters the receiver; it does not produce a copy
 #
-#   triangle(:ABC, :equilateral).rotate(30.d)
-#     # Points ABC are unaffected by the rotation; the rotated triangle is
-#     # anonymous and not even drawn.
+#   t = triangle(:ABC, :equilateral)
+#         # A, B, C are defined
+#   t.rotate(30.d)
+#         # A, B, C now have new values
+#   render
+#         # only one triangle is drawn
 #
-#   _triangle(:equilateral).rotate(30.d).register
-#     # The rotated triangle is anonymous, but is registered (will be drawn).
-#     # The original equilateral triangle will be garbage collected.
+#   t = triangle(:ABC, :equilateral)
+#   t.copy.rotate(180.d)
+#         # Here we have two triangles.  The second one has anonymous vertices.
+#   render
+#         # Two triangles will be drawn; it will look like a star.
 #
-#   _triangle(:equilateral).rotate(30.d).name(:ABC)
-#     # Points ABC are set to the rotated triangle, which is _not_ registered.
-#     # At least the points are set, so can be used to define other shapes.
+#   t = triangle(:ABC, :equilateral)
+#   t.copy.rotate(180.d).register(:XYZ)
+#   render
+#         # Our six-pointed star again, with labels X, X, A, Z, B, Y going
+#         # anti-clockwise from top.
 #
-#   _triangle(:equilateral).rotate(30.d).register(:ABC)
-#     # Equivalent to (and short for)
-#     #   _triangle(:equilateral).rotate(30.d).name(:ABC).register
+#   t = triangle(:ABC, :equilateral)
+#   t.copy.rotate(180.d).hide
+#   render
+#         # Two triangles created, but only the first one will be drawn.
+#         # The points of the second one may be used in construction.
+#         # Implementation: a shape will have a :visible flag, defaulting to
+#         # true. The renderer can just ignore invisible shapes.  This may
+#         # become the way that other shapes don't hit the register: instead of
+#         # _circle(...), we could do circle(...).hide.
 #
-# Finally, the following code will name and register the original triangle, then
-# name and register the rotated one.  Note that we rotate about A, which means A
-# is also a point in the rotated one, and we're allowed to use the point A in
-# the label.
+# Here's a way to create a nice triangle spiral (or complex star):
 #
-#   triangle(:ABC, :equilateral).rotate(30.d, :A).register(:AXY)
-#
-# Here are two approaches to creating a fully sick triangle spiral.
-#
-#   t = triangle(:A__, :equilateral)
-#   (1..35).each do |i|
-#     angle = 10*i
-#     t.rotate(angle, :A).register
-#   end
-#
-#   t = triangle(:A__, :equilateral)
+#   t = triangle(:equilateral)
 #   Shape.generate(35, t) do |_t|
-#     _t.rotate(10.d, :A).register
+#     _t.copy.rotate(10.d)
 #   end
+#
+# Rotating about one of the vertices would be pretty cool, too:
+#
+#   t = triangle(:ABC, :equilateral)
+#   Shape.generate(35, t) do |_t|
+#     _t.copy.rotate(10.d, :A)
+#   end
+#
+# How to approach this...
+#
+# * write tests based on the above scenarios
+# * flesh out #translate, #enlarge, #mirror as well
+# * think about :visible flag in Shape, and test that too
+# * also Shape#copy and Shape#register
+# * document this stuff, not just in the code
+#   - may have implications for _circle, etc.
+#   - those methods could be done by method_missing; but keep a central
+#     list of applicable methods
+# * write code in Shape class (perhaps Transformations module?)
 
-D "Shape methods" do
+xD "Shape methods" do
   D "rotate" do
     D "triangle about the origin (1)" do
       t1 = triangle(:ABC, :equilateral, :side => 3)
-      T :vertices, t1, %{ _ 0 0   _ 3 0   _ 1.5 2.59808 }   # Just making sure...
-      t2 = t1.rotate(30.d)
-      Ko t2, Triangle
-      T :vertices, t2, %{ _ 0 0   _ 2.59808 1.5   _ 0 3 }
+      T :vertices, t1, %{ A 0 0   B 3 0   C 1.5 2.59808 }   # Just making sure...
+      t1.rotate(30.d)
+      T :vertices, t1, %{ A 0 0   B 2.59808 1.5   C 0 3 }
     end
     D "triangle about the origin (2)" do
       points :D => p(2,1), :E => p(5,-1), :F => p(7,2)
       t1 = triangle(:DEF)
       T :vertices, t1, %{ D 2 1   E 5 -1   F 7 2 }   # Just making sure...
-      t2 = t1.rotate(-71.d)
+      t2 = t1.copy.rotate(-71.d)
       T :vertices, t2, %{ _ -4.47980 -0.08535   _ -5.39413 -3.57204   _ -1.90644 -4.48637 }
     end
     D "triangle about an arbitrary point" do
